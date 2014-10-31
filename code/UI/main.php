453cf7ac5aa6053be9b07d86b50e68433e9cf5ec
LeeSeung-yeon <asysa6979@gmail.com>
2014-10-31 13:18:36 +0900

    10/31 friday

---
+++ code/UI/main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	
+    <head>
+    	<title>Pivot</title>
+    	<meta http-equiv = "Content-Type" content = "text/html; charset=utf-8" />
+        <link href = "main.css" type = "text/css" rel = "stylesheet">
+    </head>
+
+    <body>
+    	<div id="top">
+    	   <a href="widget.php"><img src="up.jpg"/></a>
+    	</div>
+    	<h1>Pivot main page!</h1>
+    </body>
+</html>
