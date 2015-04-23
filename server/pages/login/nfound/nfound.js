293037b62fb355732725101ed6cbaa040723893c
LatteCom <redeye217@gmail.com>
2015-04-23 20:32:10 +0900

    구조 공사 완료

--- server/pages/login/nfound/nfound.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-	var notf = $$(".notf");
-	for(var i = 0; i<notf.length; i++){
-		notf[i].pulsate({ duration: 1});
-	}
-};
--- server/pages/signup/dup/dup.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-	var dupmsg = $$(".dupmsg");
-	for(var i = 0; i<dupmsg.length; i++){
-		dupmsg[i].pulsate({ duration: 1});
-	}
-};
