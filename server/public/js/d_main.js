246720e2693a4172fed8ee45156df3eb096b5867
Simhyejin <hj12334@naver.com>
2015-04-28 19:12:16 +0900

    memo

--- server/public/js/d_main.js
+++ server/public/js/d_main.js
@@ -404,7 +404,10 @@ app.directive('widget', function(){
     var linkFn = function(scope, element, attrs){        
         var showdel = function(e){
         	var close = this.childNodes[0].childNodes[0]; 
-        	setTimeout(function(){close.style.display="block";},500);
+        	var currentTime = new Date().getTime();
+           	while (currentTime + 500 >= new Date().getTime()) {
+           	}
+        	close.style.display="block";
         	this.bind(mouseover);
             
         };
