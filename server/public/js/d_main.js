773ea8c6aecfc0551928531bb0c39ad08c48de1b
Simhyejin <hj12334@naver.com>
2015-04-28 18:51:21 +0900

    memo

--- server/public/js/d_main.js
+++ server/public/js/d_main.js
@@ -403,7 +403,7 @@ app.directive('delmemo', function(){
 app.directive('widget', function(){
     var linkFn = function(scope, element, attrs){        
         var showdel = function(e){
-        	setTimeout(function(),1000);
+        	setTimeout(function(){},1000);
             this.childNodes[0].childNodes[0].style.display="block";   
         };
 
