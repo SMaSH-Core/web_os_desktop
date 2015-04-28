fe268030ee826df89a202da04e8a3507026459bc
Simhyejin <hj12334@naver.com>
2015-04-28 21:26:28 +0900

    route mobile

--- server/routes.js
+++ server/routes.js
@@ -1,59 +1,64 @@
-var fs = require('fs');
-var path = require('path');
-
 module.exports = function (app, passport,module){
 
 	//Request Method GET
 	app.get('/', function (req, res) {
-    	res.render('login');
+    	res.render('pages/login/login');
     });
 
-    app.get('/a', function(req,res){
-       // res.render('template/d_main_cloud_list',{fileList: req.body})
-        console.log('====temp====get');
-        console.log(req.body);
-    });
-    app.post('/template/d_main_cloud_list.ejs', function(req,res){
-        //res.render('template/d_main_cloud_list',{fileList: req.body.fileList})
-        console.log('====temp====post');
-        console.log(req.body.fileList);
-    });
+
     app.get('/login',function (req, res){
-    	res.render('login');
+    	res.render('pages/login/login');
     });
+
+     app.get('/mobile',function (req, res){
+        res.render('pages/mobile/demo/demo');
+    });
+
+	app.get('/memo',function (req, res){
+        var temp = [];
+
+        for(var i = 0; i<3; i++)
+        {
+            var newwidget = {
+                "memo": 'hieefefee'
+            }
+            temp.push(newwidget);
+        }
+
+        console.log(temp);
+
+        res.render('pages/mobile/MEMO/memo',{
+        UserID : req.user.email,
+        UserName : req.user.name,
+        widget : temp});
+    
+	});
 
     app.get('/signup',function (req, res)
     {
-    	res.render('signup');
+    	res.render('pages/signup/signup');
+    });
+
+    app.get('/mobile_main', function (req, res){
+        var wid = "{\"widget\":[]}";
+        var sessionApp = req.user.app.link;
+
+        res.render('pages/main/mobile_main',{
+        UserID : req.user.email,
+        UserName : req.user.name,
+        userapp : sessionApp,
+        widget : wid});
     });
 
     app.get('/main',module.isLoggedIn,function (req, res){
     	var wid = "{\"widget\":[]}";
         var sessionApp = req.user.app.link;
-        currentpath = './cloud/users/'+req.user.email;
-        var info = dirTree(currentpath);
-        console.log(info);
-        console.log('================');
-        console.log(res.locals);
 
-        if(res.locals.is_tablet){
-            console.log("it is tablet");
-        }
-        else if(res.locals.is_desktop){
-            console.log("it is desktop");
-        }
-        else{
-            console.log("it is mobile ")
-        }
-        
-
-    	res.render('desktop/main',{
-        	UserID : req.user.email,
-        	UserName : req.user.name,
-        	userapp : sessionApp,
-        	widget : wid,
-            local_folder : info 
-        });
+    	res.render('pages/main/main',{
+    	UserID : req.user.email,
+    	UserName : req.user.name,
+    	userapp : sessionApp,
+    	widget : wid});
     });
   
   
@@ -98,17 +103,15 @@ module.exports = function (app, passport,module){
     app.post('/widget',function (req, res){
     	console.log('post -widget');
     	console.log('parameter is ' +req.body);
-        console.log(req.body);
     });
     app.post('/app',function (req, res){
       	console.log('post -widget');
     	console.log('parameter is ' +req.body);
-        console.log(req.body);
     });
 
 
     app.get('*', function (req, res) {
-    		res.render('login');
+    		res.render('pages/login/login');
     });
 
 
@@ -122,25 +125,3 @@ module.exports = function (app, passport,module){
         res.send(req.user);
     });
 }
-
-
-    function dirTree(filename) {
-        var stats = fs.lstatSync(filename),
-            info = {
-                path: filename,
-                name: path.basename(filename)
-            };
-
-        if (stats.isDirectory()) {
-            info.type = "folder";
-            info.children = fs.readdirSync(filename).map(function(child) {
-                return dirTree(filename + '/' + child);
-            });
-        } else {
-            // Assuming it's a file. In real life it could be a symlink or
-            // something else!
-            info.type = "file";
-        }
-
-        return info;
-    }
