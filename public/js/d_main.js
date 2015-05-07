e80884a13262e34c0ef47bd623b08e2d9ba84ba0
Simhyejin <hj12334@naver.com>
2015-05-07 20:04:32 +0900

    calendar event add

--- public/js/d_main.js
+++ public/js/d_main.js
@@ -552,10 +552,18 @@ app.directive('save',function(){
 
 
 app.controller('MainSchedulerCtrl', function($scope) {
-  $scope.events = [];
+  $scope.events = [
+  	{ id:1, text:"Task A-12458",
+      start_date: new Date(2013, 10, 12),
+      end_date: new Date(2013, 10, 16) },
+    { id:2, text:"Task A-83473",
+      start_date: new Date(2013, 10, 22 ),
+      end_date: new Date(2013, 10, 24 ) }
+  ];
 
   $scope.scheduler = { date : new Date() };
   console.log($scope.events);
+
 
 });
 
