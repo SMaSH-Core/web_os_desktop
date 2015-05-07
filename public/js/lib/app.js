9be0a0bfe935882282a7b5787baa211d4eb24b12
Simhyejin <hj12334@naver.com>
2015-05-07 19:33:33 +0900

    calendar event add

--- public/js/lib/app.js
+++ public/js/lib/app.js
@@ -5,7 +5,13 @@
 var app = angular.module('schedulerApp', [ ]);
 
 app.controller('MainSchedulerCtrl', function($scope) {
-  $scope.events = [];
+  $scope.events = [ 
+  	{ id:1, text:"Task A-12458",
+      start_date: new Date(2015, 5, 12),
+      end_date: new Date(2015, 5, 16) },
+    { id:2, text:"Task A-83473",
+      start_date: new Date(2015, 5, 22 ),
+      end_date: new Date(2015, 5, 24 ) }];
   $scope.scheduler = { date : new Date() };
   console.log(events);
 
