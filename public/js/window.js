ac475551d8414ac1e4bfcbbd252d77570b0a71b5
LeeSeung-yeon <asysa6979@gmail.com>
2015-05-18 21:58:41 +0900

    안쓰는 부분 삭제

--- public/js/window.js
+++ public/js/window.js
@@ -4,29 +4,13 @@ window.onload =function (){
 // cloud part
     var popup_Btn = document.getElementById("CLOUD");
     popup_Btn.onclick = cloud_popup;
-  
-
-
 // iframe part
     var max = false;
-    var size_Btns = document.getElementsByClassName("sizeCtrl");
-    for(var i=0; i<size_Btns.length; i++){
-        size_Btns[i].onclick = function() {
-            console.log(this);
-            max = sizeControler(max, this.parentNode.parentNode);
-        };    
-    }
-    var iframe_close_Btns = document.getElementsByClassName("iframeClose");
-    for(var i=0; i<iframe_close_Btns.length;i++){
-        iframe_close_Btns[i].onclick = function(){
-            this.parentNode.parentNode.remove();
-        }
-    }
     var count=0;
     var iframe_Btns = document.getElementsByClassName("true");
     for(var i=0; i<iframe_Btns.length; i++){
         iframe_Btns[i].onclick = function() {
-            console.log(this);
+            
             var div1 = document.createElement("div");
             div1.id = "iframePopup" + count;
             div1.className = "Pstyle";
@@ -79,9 +63,4 @@ window.onload =function (){
             
         }
     }
-// topbar part
-    var Labeldropbox = document.getElementById('dropbox_label');
-    Labeldropbox.onclick = authDropbox;
-    var Labelgoogle = document.getElementById('googleDrive_label');
-    Labelgoogle.onclick = authGoogle;
 }
